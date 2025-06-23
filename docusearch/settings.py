67810d4a8c8687ad6ec74321ed110cbaa0d76ecf
ChrisOnsando <chrisnemwel@gmail.com>
2025-06-23 12:29:46 +0300

    feat(project): test firebase auth(#2)

--- docusearch/settings.py
+++ docusearch/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware', 
     'django.contrib.messages.middleware.MessageMiddleware',  
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'docusearch.middleware.FirebaseAuthMiddleware',
 ]
-MIDDLEWARE += ['docusearch.middleware.FirebaseAuthMiddleware']
 
 ROOT_URLCONF = 'docusearch.urls'
 
--- firebase_auth.py
+++ firebase_auth.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(cred)
 
 def verify_firebase_token(id_token):
     try:
